22a8b8ebfd9e46d372056c64e74050d14902ed26
Maksym Shepeta <maksim.shepeta@gmail.com>
2024-10-17 18:25:50 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
     }
 }
