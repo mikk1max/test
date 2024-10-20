5cb990adc65ad7b0df54d6791ec0eca61b1133fe
MAKSYM SHEPETA <107277579+mikk1max@users.noreply.github.com>
2024-10-20 04:38:38 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -28,6 +28,11 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
+
+        System.out.printf("Hello and welcome!");
+
+        System.out.printf("Hello and welcome!");
 
     }
 }
