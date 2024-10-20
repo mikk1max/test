254efa183dd61d0d4e96a84d3e66fb3d051c83b6
MAKSYM SHEPETA <107277579+mikk1max@users.noreply.github.com>
2024-10-20 04:34:21 +0200

    feat: add new lines
    Co-authored-by: Max <107277579+mikk1max@users.noreply.github.com>

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,7 @@ public class Main {
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!");
 
     }
 }
